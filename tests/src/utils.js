0e08750f75113ae00b43ad877631a35bcaec2bd1
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2021-03-13 07:11:44 -0300

    tests: Fix all linter issues

--- tests/src/utils.js
+++ tests/src/utils.js
@@ -1,3 +1,25 @@
+/* exported setup update has hasOnly startService
+   stopService getValueFromService */
+/* eslint no-extend-native: */
+
+/* utils.js
+ *
+ * Copyright 2020 Martin Abente Lahaye
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 imports.gi.versions.Gtk = '3.0';
 
 const {gettext} = imports;
@@ -9,7 +31,7 @@ function setup() {
     Gtk.init(null);
 
     /* XXX this shouldn't be needed */
-    const format = imports.format;
+    const {format} = imports;
     String.prototype.format = format.format;
 
     window._ = gettext.gettext;
