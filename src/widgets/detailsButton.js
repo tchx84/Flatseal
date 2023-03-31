e5cc53d8b9ea2809c615556b61af0282f49a6977
Maximiliano Sandoval R <msandova@gnome.org>
2023-03-31 11:20:43 +0200

    detailsButton: Remove can_focus
    Closes #517

--- src/widgets/detailsButton.js
+++ src/widgets/detailsButton.js
@@ -57,7 +57,6 @@ var FlatsealDetailsButton = GObject.registerClass({
 
         this.set_use_underline(true);
         this.set_label(_('_Show Details'));
-        this.can_focus = true;
         this.visible = true;
 
         this.connect('clicked', this._clicked.bind(this));
