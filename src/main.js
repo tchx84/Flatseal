38e6aad76772eb5ecd573a72598cd0b27240710f
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2023-04-12 08:59:36 -0400

    main: Fix linting issues

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ const {FlatsealApplication} = imports.application;
 const {GLib} = imports.gi;
 
 function main(argv) {
-    GLib.set_application_name("Flatseal");
+    GLib.set_application_name('Flatseal');
     const application = new FlatsealApplication();
     return application.run(argv);
 }
