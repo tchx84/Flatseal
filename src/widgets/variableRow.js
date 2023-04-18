3cd82c3c616298d8a36c26de63b774389316ad85
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2023-04-18 12:07:55 -0400

    variableRow: Remove EntryCompletion since it's not used

--- src/widgets/variableRow.js
+++ src/widgets/variableRow.js
@@ -36,7 +36,7 @@ const _notValidMsg = _('This is not a valid option');
 var FlatsealVariableRow = GObject.registerClass({
     GTypeName: 'FlatsealVariableRow',
     Template: 'resource:///com/github/tchx84/Flatseal/widgets/variableRow.ui',
-    InternalChildren: ['entry', 'button', 'store', 'image', 'statusBox'],
+    InternalChildren: ['entry', 'button', 'image', 'statusBox'],
     Properties: {
         text: GObject.ParamSpec.string(
             'text',
